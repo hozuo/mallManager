fdaee1684dcec5b7d096d818afc6b0e3cee80c73
ericson <ghzfree@163.com>
2020-04-27 17:03:12 +0800

    引入home布局

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>Home</div>
+  <el-container>
+  <el-header>Home</el-header>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
 </template>
 
 <script>
@@ -7,4 +13,26 @@ export default {}
 </script>
 
 <style>
+.el-header{
+    background-color: #B3C0D1;
+    color: #333;
+  }
+
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    height: 100%;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+  }
+
+  .el-container {
+   height: 100%;
+  }
+
 </style>
